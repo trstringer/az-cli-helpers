75b26aac763a8f0039327772445954f9c059fbb1
Thomas Stringer <thstring@microsoft.com>
2021-06-23 11:31:29 -0400

    add kubernetes arc cluster for aks-engine

--- kubernetes.sh
+++ kubernetes.sh
@@ -9,8 +9,13 @@ az_aks_engine_create_minimal () {
         return
     fi
 
-    local NAME="${RESOURCE_NAME:-$(resource_name)}"
+    if [[ -n "$RESOURCE_NAME_INTERNAL" ]]; then
+        local NAME="$RESOURCE_NAME_INTERNAL"
+    else
+        local NAME="${RESOURCE_NAME:-$(resource_name)}"
+    fi
 
+    az_group_create "$NAME"
     aks-engine deploy \
         --resource-group "$NAME" \
         --api-model "${SCRIPT_PATH}/aks-engine-minimal.json" \
@@ -26,5 +31,42 @@ az_aks_engine_create_minimal () {
     fi
     KUBECONFIG="${KUBE_CONFIG_TEMP_OLD}:${HOME}/_output/${NAME}/kubeconfig/kubeconfig.${AZLH_REGION}.json" \
         kubectl config view --flatten > "$KUBE_CONFIG_TEMP"
+    chmod 600 "$KUBE_CONFIG_TEMP"
     kubectl config use-context "$NAME"
+}
+
+az_aks_engine_arc_create_minimal () {
+    if [[ -z "$AZLH_PREFIX" ]]; then
+        echo "You must define AZLH_PREFIX"
+        return
+    fi
+
+    local NAME="${RESOURCE_NAME:-$(resource_name)}"
+    RESOURCE_NAME_INTERNAL="$NAME"
+
+    az_aks_engine_create_minimal
+
+    while true; do
+        echo "$(date) - Waiting for cluster to come up"
+        if kubectl get no; then
+            break
+        fi
+        sleep 5
+    done
+
+    while true; do
+        echo "$(date) - Attempting to connect Kubernetes cluster to Arc"
+        if az connectedk8s connect \
+                --resource-group "$NAME" \
+                --name "$NAME"; then
+            break
+        fi
+
+        # If the helm install failed, then remove it and try again.
+        az connectedk8s delete \
+            --resource-group "$NAME" \
+            --name "$NAME" \
+            --yes
+        sleep 5
+    done
 }
